619bcd1ff822ebc3af56195acdf1a12229bf589c
Tyler Compton <xaviosx@gmail.com>
2016-08-04 14:53:18 -0700

    Add a home profile.

---
+++ profiles/home-profile.sh
@@ -0,0 +1,8 @@
+# Go environment variables
+export GOPATH=/home/xavi/go
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:/home/xavi/go/bin
+export PATH=$PATH:/opt/google-appengine-go/
+
+# Misc scripts
+export PATH=$PATH:/home/xavi/sh
