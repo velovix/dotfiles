e0d49ee01bdad2d4264d6b20a8ba6599b21cd32f
Tyler Compton <xaviosx@gmail.com>
2016-08-19 14:20:31 -0700

    Add profile for desktop computer.

---
+++ profiles/home-desktop-profile.sh
@@ -0,0 +1,8 @@
+# Go environment variables
+export GOPATH=/home/velovix/go
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:/home/velovix/go/bin
+export PATH=$PATH:/opt/google-appengine-go/
+
+# Misc scripts
+export PATH=$PATH:/home/velovix/sh
