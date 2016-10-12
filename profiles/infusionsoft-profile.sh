88e7cede2a1fe17f50d5377c18333367b748ed37
Tyler Compton <xaviosx@gmail.com>
2016-10-12 16:06:42 -0700

    Remove unnecessary configurations.

--- profiles/infusionsoft-profile.sh
+++ profiles/infusionsoft-profile.sh
@@ -21,44 +21,9 @@ if [ -d "$HOME/bin" ] ; then
     PATH="$HOME/bin:$PATH"
 fi
 
-# Java setup
-export JAVA_HOME=/home/tyler/tools/jdk
-export PATH=$PATH:$JAVA_HOME/bin
-
 # Maven setup
-export MAVEN_HOME=/home/tyler/tools/maven
-export M2_HOME=$MAVEN_HOME
 export MAVEN_OPTS='-Xmx2048m -XX:CompileCommand=exclude,com/infusion/databridge/MemoryRst,loadMeta'
-export PATH=$PATH:$MAVEN_HOME/bin
-
-# Ant setup
-export ANT_HOME=/home/tyler/tools/ant
-export PATH=$PATH:$ANT_HOME/bin
-
-# Node.js Setup
-export PATH=$PATH:/home/tyler/tools/node/bin
-
-# Script directory setup
-export PATH=$PATH:/home/tyler/scripts
-
-# MySQL setup
-export PATH=$PATH:/home/tyler/tools/mysql/bin
 
 # Go setup
 export GOPATH=/home/tyler/go
 
-# IntelliJ setup
-export PATH=$PATH:/home/tyler/tools/idea/bin
-
-# MAT setup
-export PATH=$PATH:/home/tyler/tools/mat
-
-# JCA setup
-export PATH=$PATH:/home/tyler/tools/jca
-
-# VisualVM setup
-export PATH=$PATH:/home/tyler/tools/visualvim/bin
-
-# Gradle setup
-export GRADLE_HOME=/home/tyler/tools/gradle
-export PATH=$PATH:/home/tyler/tools/gradle/bin
