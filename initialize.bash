9e762e161503fe0b09e0ea8ec10300ef03c8650a
Tyler Compton <xaviosx@gmail.com>
2018-03-27 15:37:53 -0700

    Add script to link everything together

---
+++ initialize.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p $HOME/.config
+
+ln -s $PWD/xdg-config-home/nvim $HOME/.config/nvim
+ln -s $PWD/xdg-config-home/awesome $HOME/.config/awesome
+ln -s $PWD/xdg-config-home/compton.conf $HOME/.config/compton.conf
+ln -s $PWD/xdg-config-home/kitty $HOME/.config/kitty
+ln -s $PWD/xdg-config-home/lxterminal $HOME/.config/lxterminal
+ln -s $PWD/xdg-config-home/systemd $HOME/.config/systemd
+ln -s $PWD/sh $HOME/sh
+
+ln -s $PWD/zshrc $HOME/.zshrc
+ln -s $PWD/direnvrc $HOME/.direnvrc
