2fe95ea5583015980cdd86c3591a52e5224c5f2c
Tyler Compton <xaviosx@gmail.com>
2018-10-29 10:07:41 -0700

    Switch to my one-monitor setup

--- sh/monitor-checker.bash
+++ sh/monitor-checker.bash
@@ -11,9 +11,8 @@ then
 	echo "Swiching to external"
 	LAST_SETTING="external"
 
-	xrandr --output eDP-1-1 --dpi 96 --auto \
+	xrandr --output eDP-1-1 --off \
 		--output HDMI-0 --dpi 96 --primary --right-of eDP-1-1 --auto \
-		--output DP-0 --dpi 96 --right-of HDMI-0 --auto
 elif [ "$LINE_COUNT" == 0 ] && [ "$LAST_SETTING" != "no external" ]
 then
 	echo "Switching to no-external"
