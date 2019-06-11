5746f9b218cd9e58a0d5742d91753e02f855c8b5
Tyler Compton <xaviosx@gmail.com>
2019-06-11 10:53:03 -0700

    Add symlink for rofi configuration

--- initialize.bash
+++ initialize.bash
@@ -8,6 +8,7 @@ ln -s $PWD/xdg-config-home/compton.conf $HOME/.config/compton.conf
 ln -s $PWD/xdg-config-home/kitty $HOME/.config/kitty
 ln -s $PWD/xdg-config-home/lxterminal $HOME/.config/lxterminal
 ln -s $PWD/xdg-config-home/pacman $HOME/.config/pacman
+ln -s $PWD/xdg-config-home/rofi $HOME/.config/rofi
 ln -s $PWD/sh $HOME/sh
 
 ln -s $PWD/zshrc $HOME/.zshrc
